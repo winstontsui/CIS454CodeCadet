c3f00527e0d263dfa58d4d42a1f0d2d03ee0c6d1
SandyLinGitHub <slin45@syr.edu>
2023-10-16 03:24:20 -0400

    polished view.py

--- CodeCadetWebsite/src/personal/views.py
+++ CodeCadetWebsite/src/personal/views.py
@@ -1,38 +1,3 @@
-'''
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
-from django.contrib.auth.models import User
-from .forms import NewUserForm
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
-
-from django.contrib.sites.shortcuts import get_current_site
-from django.shortcuts import render, redirect
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
-from django.template.loader import render_to_string
-from personal.forms import SignUpForm
-from personal.tokens import account_activation_token
-
-def home_view(request, *args, **kwargs):
-    return render(request, "home.html", {})
-
-def register_request(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("main:homepage")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render (request, 'register.html')
-'''
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
