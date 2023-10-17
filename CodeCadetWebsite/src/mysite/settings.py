e0125024942ab7888288457ee907091388f37ff4
SandyLinGitHub <slin45@syr.edu>
2023-10-17 00:51:16 -0400

    image

--- CodeCadetWebsite/src/mysite/settings.py
+++ CodeCadetWebsite/src/mysite/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFULES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
